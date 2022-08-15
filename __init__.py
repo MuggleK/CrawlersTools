c40d630761cefd3050a48d0bae13197f4f96e0b3
changchuan.pei <775847312@qq.com>
2022-08-15 15:39:19 +0800

    init commit

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Project : CrawlersTools
-# @Time    : 2022/8/12 20:48
-# @Author  : Changchuan.Pei
-# @File    : __init__.py
-
-def function():
-    print("hello from CrawlersTools/__init__.py")
