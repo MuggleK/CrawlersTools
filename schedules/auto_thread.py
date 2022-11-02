b2f73815aa4021ab6f458d77554a67b50f72b178
changchuan.pei <changchuan.pei@qizhidao.com>
2022-11-02 15:15:40 +0800

    优化动态线程

--- schedules/auto_thread.py
+++ schedules/auto_thread.py
@@ -7,38 +7,30 @@
 import time
 import random
 
+from threading import Thread, active_count, Lock
+from traceback import format_exc
+
 from loguru import logger
-import threading
-import traceback
+
+thread_lock = Lock()
 
 
-class ExcThread(threading.Thread):
+class ExcThread(Thread):
     """
     主动捕获子线程异常
     """
     def __init__(self, target, args=(), kwargs=None):
         super(ExcThread, self).__init__()
-        self.function = target
-        self.args = args
-        self.kwargs = kwargs if kwargs else {}
-        self.exit_code = 0
-        self.exception = None
-        self.exc_traceback = ''
+        self._target = target
+        self._args = args
+        self._kwargs = kwargs if kwargs else {}
 
     def run(self):
         try:
-            self._run()
-        except Exception as e:
-            self.exit_code = 1
-            self.exception = e
-            self.exc_traceback = traceback.format_exc()
-
-    def _run(self):
-        try:
-            self.function(*self.args, **self.kwargs)
-        except Exception as e:
-            logger.error(f'args：{self.args} kwargs：{self.kwargs}，{traceback.format_exc(limit=3)}')
-            raise e
+            if self._target:
+                self._target(*self._args, **self._kwargs)
+        except:
+            logger.error(f'args：{self._args} kwargs：{self._kwargs}，{format_exc()}')
 
 
 class AutoThread(object):
@@ -54,65 +46,35 @@ class AutoThread(object):
         self.thread_num = thread_num
         self.fun = fun
         self.arg_list = arg_list
-        self.thread_lock = threading.Lock()
-        self.loop_counter = 0
+        self.os_threads = active_count()
 
-    def fun_thread(self):
-        """
-        并发函数，为每个函数动态创建线程
-        :return:
-        """
-        while 1:
-            active_thread = threading.active_count()
-            # logger.debug(f'当前主线程循环次数：{self.loop_counter}  限制线程总数：{self.thread_num} 当前存活线程数：{active_thread}')
-            if active_thread < self.thread_num:
-                for _ in range(self.thread_num - active_thread):
-                    self.thread_lock.acquire()
-                    task_fun = self.fun.pop() if self.fun else None
-                    self.thread_lock.release()
-                    if task_fun:
-                        t = ExcThread(target=task_fun, args=(self.arg_list,)) if self.arg_list else ExcThread(target=task_fun)  # 注意传入的参数一定是一个元组!
-                        t.start()
-                        if t.exit_code:
-                            logger.error(t.exc_traceback)
+    def task_status(self):
+        return self.fun if isinstance(self.fun, list) else self.arg_list
 
-            self.loop_counter += 1
-            time.sleep(random.uniform(0, 2))
-            if not self.fun:
-                # logger.info("Main Thread Done")
-                return
-
-    def arg_thread(self):
-        """
-        并发传参，为每个参数动态创建线程
-        :return:
-        """
-        while 1:
-            active_thread = threading.active_count()
-            # logger.debug(f'当前主线程循环次数：{self.loop_counter}  限制线程总数：{self.thread_num} 当前存活线程数：{active_thread}')
-            if active_thread < self.thread_num:
-                for _ in range(self.thread_num - active_thread):
-                    self.thread_lock.acquire()
-                    task_arg = self.arg_list.pop() if self.arg_list else None
-                    self.thread_lock.release()
-                    if task_arg:
-                        t = ExcThread(target=self.fun, args=(task_arg, ))  # 注意传入的参数一定是一个元组!
-                        t.start()
-                        if t.exit_code:
-                            logger.error(t.exc_traceback)
-
-            self.loop_counter += 1
-            time.sleep(random.uniform(0, 2))
-            if not self.arg_list:
-                # logger.info("Main Thread Done")
-                return
+    def wait(self):
+        while active_count() > self.os_threads:
+            time.sleep(.25)
 
     def main_thread(self):
-        """
+        while self.task_status():
+            active_thread = active_count()
+            if active_count() >= self.thread_num:
+                time.sleep(random.uniform(0, 1))
+                continue
+            for _ in range(self.thread_num - active_thread + self.os_threads):
+                thread_lock.acquire()
+                if isinstance(self.fun, list):
+                    task_fun = self.fun.pop() if self.fun else None
+                    thread_lock.release()
+                    if not task_fun:
+                        break
+                    child_thread = ExcThread(target=task_fun, args=(self.arg_list,)) if self.arg_list else ExcThread(
+                        target=task_fun)  # 注意传入的参数一定是一个元组!
+                else:
+                    task_arg = self.arg_list.pop() if self.arg_list else None
+                    thread_lock.release()
+                    if not (task_arg or task_arg == 0):
+                        break
+                    child_thread = ExcThread(target=self.fun, args=(task_arg,))  # 注意传入的参数一定是一个元组!
 
-        :return:
-        """
-        if isinstance(self.fun, list):
-            self.fun_thread()
-        else:
-            self.arg_thread()
+                child_thread.start()
