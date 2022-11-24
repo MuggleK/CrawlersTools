8f89355a0bb1c78f205383310c80ad2a9a8e62b7
changchuan.pei <changchuan.pei@qizhidao.com>
2022-11-24 10:10:50 +0800

    优化任务队列为生成器

--- schedules/auto_thread.py
+++ schedules/auto_thread.py
@@ -7,7 +7,6 @@
 import time
 import random
 from inspect import isgenerator
-
 from threading import Thread, active_count, Lock
 from traceback import format_exc
 
@@ -20,6 +19,7 @@ class ExcThread(Thread):
     """
     主动捕获子线程异常
     """
+
     def __init__(self, target, args=(), kwargs=None):
         super(ExcThread, self).__init__()
         self._target = target
@@ -36,19 +36,20 @@ class ExcThread(Thread):
 
 class AutoThread(object):
     """
-    动态创建线程
+    动态线程调度, 传入任务队列可为列表（初始化转换成生成器），也可为生成器
     usage:
         a_thread = AutoThread(20, fun, arg_list)
         a_thread.main_thread()
 
     ps: 支持两种并发方式：1.并发函数 2.并发传参
     """
+
     def __init__(self, thread_num: int, fun, arg_list=None):
         self.thread_num = thread_num
-        self.arg = None if arg_list is None else arg_list  # 并发函数时传参
+        self.arg = arg_list  # 并发函数需要入参
         self.fun = (f for f in fun) if isinstance(fun, list) else fun
         self.args = (arg for arg in arg_list) if isinstance(arg_list, list) else arg_list
-        self.flag = True if isgenerator(self.fun) else False     # True 并发函数 False并发参数
+        self.flag = True if isgenerator(self.fun) else False  # True 并发函数 False并发参数
         self.os_threads = active_count()
 
     def wait(self):
@@ -76,16 +77,17 @@ class AutoThread(object):
                 if self.flag:
                     task_fun = self.next_task(self.fun)
                     thread_lock.release()
-                    if not task_fun:
+                    if task_fun is None:
                         loop_flag = False
                         break
-                    child_thread = ExcThread(target=task_fun) if self.arg is None else ExcThread(target=task_fun, args=(self.arg,))  # 注意传入的参数一定是一个元组!
+                    child_thread = ExcThread(target=task_fun) if self.arg is None else ExcThread(target=task_fun, args=(
+                        self.arg,))
                 else:
                     task_arg = self.next_task(self.args)
                     thread_lock.release()
-                    if not (task_arg or task_arg == 0):
+                    if task_arg is None:
                         loop_flag = False
                         break
-                    child_thread = ExcThread(target=self.fun, args=(task_arg,))  # 注意传入的参数一定是一个元组!
+                    child_thread = ExcThread(target=self.fun, args=(task_arg,))
 
                 child_thread.start()
