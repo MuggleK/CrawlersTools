77d2a01c0102a8b71a8296a2db85c0aa37795f41
changchuan.pei <changchuan.pei@qizhidao.com>
2023-03-21 15:17:57 +0800

    update auto_thread

--- CrawlersTools/schedules/auto_thread.py
+++ CrawlersTools/schedules/auto_thread.py
@@ -5,12 +5,12 @@
 # @File    : auto_thread.py
 
 import time
-import random
 from inspect import isgenerator
 from threading import Thread, active_count, Lock
 from traceback import format_exc
 
 from loguru import logger
+
 
 thread_lock = Lock()
 
@@ -20,18 +20,18 @@ class ExcThread(Thread):
     主动捕获子线程异常
     """
 
-    def __init__(self, target, args=(), kwargs=None):
+    def __init__(self, target, args=None, kwargs=None):
         super(ExcThread, self).__init__()
         self._target = target
-        self._args = args
-        self._kwargs = kwargs if kwargs else {}
+        self._args = tuple() if (args or args == (None, )) else args
+        self._kwargs = kwargs or dict()
 
     def run(self):
         try:
             if self._target:
                 self._target(*self._args, **self._kwargs)
         except:
-            logger.error(f'args：{self._args} kwargs：{self._kwargs}，{format_exc()}')
+            logger.error(f'target：{self._target} args：{self._args} kwargs：{self._kwargs}，{format_exc()}')
 
 
 class AutoThread(object):
@@ -41,15 +41,13 @@ class AutoThread(object):
         a_thread = AutoThread(20, fun, arg_list)
         a_thread.main_thread()
 
-    ps: 支持两种并发方式：1.并发函数 2.并发传参
+    ps: 支持三种并发方式：1.并发函数 2.并发传参 3.函数和参数按顺序组合
     """
 
     def __init__(self, thread_num: int, fun, arg_list=None):
         self.thread_num = thread_num
-        self.arg = arg_list  # 并发函数需要入参
         self.fun = (f for f in fun) if isinstance(fun, list) else fun
-        self.args = (arg for arg in arg_list) if isinstance(arg_list, list) else arg_list
-        self.flag = True if isgenerator(self.fun) else False  # True 并发函数 False并发参数
+        self.args = (a for a in arg_list) if isinstance(arg_list, list) else arg_list
         self.os_threads = active_count()
 
     def wait(self):
@@ -66,28 +64,17 @@ class AutoThread(object):
         return task_arg
 
     def main_thread(self):
-        loop_flag = True
-        while loop_flag:
-            active_thread = active_count()
-            if active_count() >= self.thread_num:
-                time.sleep(random.uniform(0, 1))
-                continue
-            for _ in range(self.thread_num - active_thread + self.os_threads):
-                thread_lock.acquire()
-                if self.flag:
-                    task_fun = self.next_task(self.fun)
-                    thread_lock.release()
-                    if task_fun is None:
-                        loop_flag = False
-                        break
-                    child_thread = ExcThread(target=task_fun) if self.arg is None else ExcThread(target=task_fun, args=(
-                        self.arg,))
-                else:
-                    task_arg = self.next_task(self.args)
-                    thread_lock.release()
-                    if task_arg is None:
-                        loop_flag = False
-                        break
-                    child_thread = ExcThread(target=self.fun, args=(task_arg,))
+        while True:
+            with thread_lock:
+                active_thread = active_count()
+                if active_thread >= self.thread_num:
+                    time.sleep(.5)
+                    continue
 
-                child_thread.start()
+            fun = self.next_task(self.fun) if isgenerator(self.fun) else self.fun
+            args = self.next_task(self.args) if isgenerator(self.args) else self.args
+            if fun is None and args is None:
+                break
+
+            t = ExcThread(target=fun, args=(args,))
+            t.start()
