97ece030fdc4aff4c05cecd76937a9f35c7c0b69
changchuan.pei <changchuan.pei@qizhidao.com>
2023-04-17 14:36:05 +0800

    empty time string

--- CrawlersTools/preprocess/time_process.py
+++ CrawlersTools/preprocess/time_process.py
@@ -9,6 +9,8 @@ from datetime import datetime, timedelta
 
 from sinan import Sinan
 
+from CrawlersTools.projects.filters import empty_title
+
 
 class TimeProcessor:
 
@@ -18,6 +20,7 @@ class TimeProcessor:
         self.fmt = "%Y-%m-%d"   # 暂时只处理年月日
 
     def format(self, string, struct=False):
+        string = empty_title(string)
         try:
             return self.process_timestamp(string, struct)
         except ValueError:
